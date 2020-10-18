f3dfc47646bccb9c77d4684dbc2a277ac9b613bf
Sam Smith <sam@example.com>
2020-10-18 16:13:22 +0300

    watchlist changes

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-
-    DEBUG = True
-    pass
-
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    print(MOVIE_API_KEY)
 
 
 class ProdConfig(Config):
     '''
     Production  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
     '''
@@ -22,12 +22,13 @@ class ProdConfig(Config):
 class DevConfig(Config):
     '''
     Development  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
     '''
 
+    DEBUG = True
 
-
-   
-    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='a95b92530ad60db0b4bb9f256916e57c'
+export SECRET_KEY=<Your secret key>
+
+python3.6 manage.py server
